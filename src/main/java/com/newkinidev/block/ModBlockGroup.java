c117dc3afb35b426feffea06c16658729e2b999e
newkincode <maainnewkin59@gmail.com>
2023-07-28 13:47:46 +0900

    Revert "포지는 별로다 역시"
    This reverts commit 9db9fe82b9cbb10b2cb0e9ef32ab66bee60efc69.

---
+++ src/main/java/com/newkinidev/block/ModBlockGroup.java
@@ -0,0 +1,34 @@
+package com.newkinidev.block;
+
+import com.newkinidev.MineLife;
+import com.newkinidev.item.ModItems;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModBlockGroup {
+    // Define your custom ItemGroup
+    public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModBlocks.SUPPORT_BLOCK))
+            .displayName(Text.translatable("minelife.block"))
+            .build();
+
+    // Define the RegistryKey for your custom ItemGroup
+    public static final RegistryKey<ItemGroup> ITEM_GROUP_KEY = RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(MineLife.MOD_ID, "block"));
+
+    private static void registerGroup() {
+        // Register the custom ItemGroup with the defined RegistryKey
+        Registry.register(Registries.ITEM_GROUP, ITEM_GROUP_KEY.getValue(), ITEM_GROUP);
+    }
+
+    public static void registerModGroups() {
+        MineLife.LOGGER.info("Registering Mod Groups for " + MineLife.MOD_ID);
+        registerGroup();
+    }
+}
---
+++ src/main/java/com/newkinidev/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.newkinidev.item;
+
+import com.newkinidev.MineLife;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item MAGNIFIER = new Item(new FabricItemSettings());
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(MAGNIFIER);
+    }
+
+    private static void registerItem() {
+        Registry.register(Registries.ITEM, new Identifier(MineLife.MOD_ID,"magnifier"), MAGNIFIER);
+    }
+
+    public static void registerModItems() {
+        MineLife.LOGGER.info("Registering Mod Items for " + MineLife.MOD_ID);
+        registerItem();
+
+        ItemGroupEvents.modifyEntriesEvent(ModItemGroup.ITEM_GROUP_KEY).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
